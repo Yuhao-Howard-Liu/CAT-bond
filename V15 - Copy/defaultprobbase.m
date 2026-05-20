4c4cd19867dfdad96f161e8b10d6579b9c94a4fa
Yuhao-Howard-Liu <59323490+Yuhao-Howard-Liu@users.noreply.github.com>
2026-05-20 16:34:25 +1000

    Delete V15 - Copy directory

--- V15 - Copy/defaultprobbase.m
+++
@@ -1,4 +0,0 @@
-function [x] = defaultprobbase(T,t,s,y0,rho,ptimes,etimes)
-%
-x=condexpect1(T,y0,t,s,[1 0 0],rho,ptimes,etimes);
-end
--- V15 - Copy/thetastar1.m
+++
@@ -1,5 +0,0 @@
-function [tt] = thetastar1(state)
-% first  elements of theta i.e. lambda
-tt= theta1(state)+gamma1(state)*sigma1(state)/g1(state);
-end
-
